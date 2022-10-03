d2814179268a16d5d1aeb2f79b78985e212537c2
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-10-03 21:13:13 +0000

    Add tqdm, only train on non-truncated terms, new defaults

--- test_train.py
+++ test_train.py
@@ -2,16 +2,31 @@
 from coq2vec import CoqTermRNNVectorizer
 import random
 import os
-with open('/home/alex/coq2vec/terms.txt', 'r') as f:
-    terms = [l.strip() for l in f]
+from typing import List
+import re
+from tqdm import tqdm
+import itertools
+
+symbols_regexp = (r',|(?::>)|(?::(?!=))|(?::=)|\)|\(|;|@\{|~|\+{1,2}|\*{1,2}|&&|\|\||'
+                  r'(?<!\\)/(?!\\)|/\\|\\/|(?<![<*+-/|&])=(?!>)|%|(?<!<)-(?!>)|'
+                  r'<-|->|<=|>=|<>|\^|\[|\]|(?<!\|)\}|\{(?!\|)')
+def get_symbols(string: str) -> List[str]:
+    return [word for word in re.sub(
+        r'(' + symbols_regexp + ')',
+        r' \1 ', string).split()
+            if word.strip() != '']
+
+max_length=30
+with open('800000-samples-terms.txt', 'r') as f:
+    terms = list(tqdm(itertools.islice((l.strip() for l in tqdm(f, total=80000) if len(get_symbols(l)) < max_length - 1), 80000)))
 vectorizer = CoqTermRNNVectorizer()
 # 804857
 os.makedirs("weights", exist_ok=True)
-for epoch, _ in enumerate(vectorizer.train(random.sample(terms, 80000), hidden_size=512, learning_rate=0.81,
-                                           n_epochs=12, batch_size=64, print_every=32,
-                                           gamma=0.82, momentum=0.86,
+for epoch, _ in enumerate(vectorizer.train(terms, hidden_size=3712, learning_rate=0.32,
+                                           n_epochs=60, batch_size=64, print_every=16,
+                                           gamma=0.9, momentum=0.86,
                                            force_max_length=30, epoch_step=1,
-                                           num_layers=1, teacher_forcing_ratio=0.3, verbosity=1)):
+                                           num_layers=2, teacher_forcing_ratio=0.38, verbosity=1)):
     weightspath = f"weights/term2vec-weights-{epoch}.dat"
     print(f"Saving weights to {weightspath}")
     vectorizer.save_weights(weightspath)
