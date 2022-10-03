5a89d50dc774cc1c0142497ea7bba171314c5ec5
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-10-03 21:06:37 +0000

    Use the 80000 samples file to test the same samples as other phases in the process, load more efficiently, and add progress bar

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -3,6 +3,8 @@ from coq2vec import CoqTermRNNVectorizer, tune_termrnn_hyperparameters
 from typing import List
 import random
 import re
+from tqdm import tqdm
+import itertools
 
 symbols_regexp = (r',|(?::>)|(?::(?!=))|(?::=)|\)|\(|;|@\{|~|\+{1,2}|\*{1,2}|&&|\|\||'
                   r'(?<!\\)/(?!\\)|/\\|\\/|(?<![<*+-/|&])=(?!>)|%|(?<!<)-(?!>)|'
@@ -14,9 +16,10 @@ def get_symbols(string: str) -> List[str]:
             if word.strip() != '']
 
 max_length = 20
+num_terms = 20484
 
-with open('/home/alex/coq2vec/terms.txt', 'r') as f:
-    terms = [l.strip() for l in f if len(get_symbols(l)) < max_length - 1]
+with open('800000-samples-terms.txt', 'r') as f:
+    terms = list(tqdm(itertools.islice((l.strip() for l in f if len(get_symbols(l)) < max_length - 1),num_terms), total=num_terms))
 
-tune_termrnn_hyperparameters(random.sample(terms, 5242), n_epochs=20, batch_size=32,
+tune_termrnn_hyperparameters(terms, n_epochs=20, batch_size=32,
                              print_every=16, force_max_length=max_length, epoch_step=1)
