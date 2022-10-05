909a3087031c7a798ec14a9e5b16827a3d8a7399
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-10-05 20:26:25 +0000

    More accurate term pruning for length

--- test_train.py
+++ test_train.py
@@ -7,18 +7,9 @@ import re
 from tqdm import tqdm
 import itertools
 
-symbols_regexp = (r',|(?::>)|(?::(?!=))|(?::=)|\)|\(|;|@\{|~|\+{1,2}|\*{1,2}|&&|\|\||'
-                  r'(?<!\\)/(?!\\)|/\\|\\/|(?<![<*+-/|&])=(?!>)|%|(?<!<)-(?!>)|'
-                  r'<-|->|<=|>=|<>|\^|\[|\]|(?<!\|)\}|\{(?!\|)')
-def get_symbols(string: str) -> List[str]:
-    return [word for word in re.sub(
-        r'(' + symbols_regexp + ')',
-        r' \1 ', string).split()
-            if word.strip() != '']
-
 max_length=30
 with open('800000-samples-terms.txt', 'r') as f:
-    terms = list(tqdm(itertools.islice((l.strip() for l in tqdm(f, total=80000) if len(get_symbols(l)) < max_length - 1), 80000)))
+    terms = list(tqdm(itertools.islice((l.strip() for l in tqdm(f, total=80000) if vectorizer.term_seq_length(l) < max_length - 1), 80000)))
 vectorizer = CoqTermRNNVectorizer()
 # 804857
 os.makedirs("weights", exist_ok=True)
